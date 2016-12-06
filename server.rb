21436d31946ccaef34913df4129d808d62604ac2
WhitepagesPro <chales@whitepages.com>
2016-12-06 14:28:42 -0800

    Test

--- server.rb
+++ server.rb
@@ -1,6 +1,6 @@
-require_relative 'twilio-ruby/lib/twilio-ruby.rb'
 require 'sinatra'
 require 'sinatra/json'
+require 'twilio-ruby'
 
 disable :protection
 
@@ -14,7 +14,6 @@ appsid      = ENV['twilio_app_id']
 api_key     = ENV['twilio_api_key']
 api_secret  = ENV['twilio_api_secret']
 sync_sid    = ENV['twilio_sync_service_sid']
-
 
 get '/' do
     client_name = params[:client]
@@ -46,47 +45,26 @@ get '/token' do
   grant.service_sid = sync_sid
   grant.endpoint_id = endpoint_id
   token.add_grant grant
-
   # Generate the token and send to the client
   json :identity => identity, :token => token.to_jwt
-end
-
-post '/dial' do
-    #determine if call is inbound
-    number = params[:PhoneNumber]
-
-    response = Twilio::TwiML::Response.new do |r|
-        # Should be your Twilio Number or a verified Caller ID
-        r.Dial :callerId => caller_id do |d|
-            # Test to see if the PhoneNumber is a number, or a Client ID. In
-            # this case, we detect a Client ID by the presence of non-numbers
-            # in the PhoneNumber parameter.
-            if /^[\d\+\-\(\) ]+$/.match(number)
-                d.Number(CGI::escapeHTML number)
-            else
-                d.Client number
-            end
-        end
-    end
-    response.text
 end
 
 #this will be called from a Twilio voice URL
 #for inbound calls, dial the default_client
 post '/inbound' do
-
     from = params[:From]
     addOnData = params[:AddOns]
     client = Twilio::REST::Client.new(account_sid, auth_token)
     # Sending the add on data through Twilio Sync
     service = client.preview.sync.services(sync_sid)
-    response = service.documents("TwilioChannel").update(data: addOnData)
+    service.documents("TwilioChannel").update(data: addOnData)
     # Dials the default_client
-    response2 = Twilio::TwiML::Response.new do |r|
+    response = Twilio::TwiML::Response.new do |r|
+        r.Say("Spam Beware, please wait for the next available agent ")
         # Should be your Twilio Number or a verified Caller ID
         r.Dial :callerId => from do |d|
             d.Client default_client
         end
     end
-    response2.text
+    response.text
 end
