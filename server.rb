19169dc345e86a94fa7df49d2ec613cecb944b6c
WhitepagesPro <chales@whitepages.com>
2016-12-06 15:22:53 -0800

    Test

--- server.rb
+++ server.rb
@@ -63,13 +63,12 @@ post '/inbound' do
     service = client.preview.sync.services(sync_sid)
     service.documents("TwilioChannel").update(data: addOnData)
     # Dials the default_client
-    response = Twilio::TwiML::Response.new do |r|
-        r.Say("Spam Beware, please wait for the next available agent ")
+    Twilio::TwiML::Response.new do |r|
+      #  r.Say("Spam Beware, please wait for the next available agent ")
         r.Enqueue :workflowSid => wFlow_sid
         # Should be your Twilio Number or a verified Caller ID
-        r.Dial :callerId => from do |d|
-           d.Client default_client
-        end
+      #  r.Dial :callerId => from do |d|
+      #     d.Client default_client
+      #  end
     end.text
-    response.text
 end
