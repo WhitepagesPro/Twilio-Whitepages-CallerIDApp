2f8bf9197d24d2f50ded2ec5aca4571a48222376
WhitepagesPro <chales@whitepages.com>
2016-12-07 09:27:21 -0800

    Test

--- server.rb
+++ server.rb
@@ -19,7 +19,6 @@ wSpace_sid  = ENV['twilio_workspace_sid']
 wFlow_sid   = ENV['twilio_workflow_sid']
 activ_sid   = ENV['twilio_activity_sid']
 
-
 #trClient = Twilio::REST::Client.new(account_sid, auth_token, wSpace_sid)
 
 get '/' do
